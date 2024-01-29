5c1db4b4038dd1de8207c3926798c54c1154515b
Saska Radosavljevic <sashka.radosavljevic@gmail.com>
2024-01-29 21:37:46 +0100

    Initial Commit

---
+++ FinnAdBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinnAdBrowser
+//
+//  Created by Saska Radosavljevic on 29/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinnAdBrowser/FinnAdBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinnAdBrowserApp.swift
+//  FinnAdBrowser
+//
+//  Created by Saska Radosavljevic on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinnAdBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
